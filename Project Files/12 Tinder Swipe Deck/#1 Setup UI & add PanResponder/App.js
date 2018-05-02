74316c03cad9f4d66cee4eddad856897d2db7acc
Varun Nath <nathvarun@hotmail.com>
2018-05-02 19:37:01 +0530

    Tinder Swipe Deck #1 Setup & Add PanResponder

---
+++ Project Files/12 Tinder Swipe Deck/#1 Setup UI & add PanResponder/App.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height
+const SCREEN_WIDTH = Dimensions.get('window').width
+const Users = [
+  { id: "1", uri: require('./assets/1.jpg') },
+  { id: "2", uri: require('./assets/2.jpg') },
+  { id: "3", uri: require('./assets/3.jpg') },
+  { id: "4", uri: require('./assets/4.jpg') },
+  { id: "5", uri: require('./assets/5.jpg') },
+]
+
+export default class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.position = new Animated.ValueXY()
+    this.state = {
+      currentIndex: 0
+    }
+  }
+  componentWillMount() {
+    this.PanResponder = PanResponder.create({
+
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+
+        this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+
+      }
+    })
+  }
+
+  renderUsers = () => {
+
+    return Users.map((item, i) => {
+
+      return (
+        <Animated.View
+          {...this.PanResponder.panHandlers}
+          key={item.id} style={[{ transform: this.position.getTranslateTransform() }, { height: SCREEN_HEIGHT - 120, width: SCREEN_WIDTH, padding: 10, position: 'absolute' }]}>
+
+          <Image
+            style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 20 }}
+            source={item.uri} />
+
+        </Animated.View>
+      )
+    }).reverse()
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ height: 60 }}>
+
+        </View>
+        <View style={{ flex: 1 }}>
+          {this.renderUsers()}
+        </View>
+        <View style={{ height: 60 }}>
+
+        </View>
+
+
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
